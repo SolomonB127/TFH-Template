5a7f7911c1cdba288ec656c8f44b0b0d52ec8566
Akinsulire Olabode Solomon <solomonakinsulire@gmail.com>
2025-03-04 13:38:11 +0000

    feat: Integrate Hero and Event components into Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
 import AppBanner from "./components/AppBanner";
+import Hero from "./components/Hero";
+import Event from "./components/Event";
 
 export default function Home() {
   return (
     <>
       <AppBanner />
+      <Hero />
+      <Event />
     </>
   );
 }
